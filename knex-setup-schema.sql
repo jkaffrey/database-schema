411ad80743e155401acd8e5808d0d9c0e1c3f1ed
rogerwschmidt <rogerschmidt@gmail.com>
2016-06-20 09:42:20 -0600

    adding schema

---
+++ knex-setup-schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE students (
+  id  serial,
+  first_name varchar(255),
+  last_name varchar(255),
+  hobby varchar(255)
+);
+
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Roger', 'Schmidt', 'kiteboarding');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Jimmy', 'Stafford', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Gabe', 'Thexton', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Kristen', 'Foster-Marks', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Jamie', 'Thorson', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Jeremy', 'Kaffrey', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Jess', 'Morris', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Michael', 'Hottman', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Mike', 'Meserau', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Phillip', 'Reitz', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Roy', 'Chase', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Ryan', 'Mackey', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Sam', 'Cooksey', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Stepahine', 'Ogburn', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Vy', 'Schmidt', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Roger', 'Nguyen', '');
+INSERT INTO students (first_name, last_name, hobby) VALUES ('Daniel', 'Dwiyer', '');
